053c738fb3b5dab9eb696691e8f3e68a07a2ba8c
Andrei Leone Parente Silva <andreiparente@gmail.com>
2016-06-03 21:54:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -16,15 +16,15 @@ enum gameState
 	STATE_OPTIONS,
 	STATE_PAUSE
 };
-	
+
 // Inicializa o SDL e cria janela
 void init_boot_game();
+	
+// Carrega imagem PNG como textura
+void load_Texture( char* l_Path );
 
-// Carrega e otimiza imagem PNG 
-SDL_Surface* load_and_optimizeSurface( char* l_Path );
-
-//Aplica e atualiza a surface
-void apply_update_Surface (SDL_Surface* l_Surface);
+//Aplica e atualiza a textura
+void apply_Texture();
 
 // Limpa memoria e fecha o SDL e o jogo
 int close_game();
@@ -32,8 +32,11 @@ int close_game();
 // A janela do jogo
 SDL_Window* g_Window = NULL;
 
-// Surface que vai conter nossa janela
-SDL_Surface* g_ScreenSurface = NULL;
+// O renderizador da janela
+SDL_Renderer* g_Renderer = NULL;
+
+// Textura atual sendo apresentada
+SDL_Texture* g_Texture = NULL;
 
 /*========================================================================================================================================*/
 /*==================================================================SEPARANDO=============================================================*/
@@ -50,7 +53,7 @@ void init_boot_game()
 	else
 	{
 		//Cria a janela
-		g_Window = SDL_CreateWindow( "Peleh Legacy v0.03", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		g_Window = SDL_CreateWindow( "Peleh Legacy v0.04", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( g_Window == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
@@ -58,26 +61,36 @@ void init_boot_game()
 		
 		else
 		{
-			// Inicializa o carregamento de PNG
-			int imgFlags = IMG_INIT_PNG;
-			if( !( IMG_Init( imgFlags ) & imgFlags ) )
+			// Cria renderizador para a janela
+			g_Renderer = SDL_CreateRenderer( g_Window, -1, SDL_RENDERER_ACCELERATED );
+			if( g_Renderer == NULL )
 			{
-				printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
 			}
 			
 			else
 			{
-				// Pegando a surface da janela
-				g_ScreenSurface = SDL_GetWindowSurface( g_Window );
+				//Inicializa a cor do renderizador
+				SDL_SetRenderDrawColor( g_Renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				
+				// Inicializa o carregamento de PNG
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+				}
 			}
 		}
 	}
-	// Carregando imagem PNG da empresa e otimiza, depois aplica a surface e a atualiza
-	apply_update_Surface ( load_and_optimizeSurface( "C:\Users\User\Desktop\Facul\Lazy Foo\meu_jogo\empresa.png" ) );
+	
+	// Carrega a imagem PNG da empresa em textura, depois aplica a textura e a atualiza a tela
+	load_Texture( "empresa.png" );
+	apply_Texture();
 	SDL_Delay( 3000 );
 	
-	// Carregando imagem PNG dos creditos ao SDL2.0 e otimiza, depois aplica a surface e a atualiza
-	apply_update_Surface ( load_and_optimizeSurface( "C:\Users\User\Desktop\Facul\Lazy Foo\meu_jogo\sdlcredits.png" ) );
+	// Carrega a imagem PNG dos creditos ao SDL 2.0 em textura, depois aplica a textura e a atualiza a tela
+	load_Texture( "sdlcredits.png" );
+	apply_Texture();
 	SDL_Delay( 3000 );
 
 	return sucess;
@@ -85,16 +98,16 @@ void init_boot_game()
 
 int main(int argc, char *argv[])
 {    	
-	// Testando init_boot_game
-	if( !init_boot_game() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
+	// Iniciando o jogo
+	init_boot_game() ;
 	
 	else
 	{
 		// Flag do loop principal
 		int quit_game = 0;
+		
+		// Flag do loop principal
+		int l_quit_game = 0;
 		
 		// Cuidador de evento
 		SDL_Event e;
@@ -103,7 +116,7 @@ int main(int argc, char *argv[])
 		gameState = STATE_TITLE_SCREEN;
 
 		// Enquanto a aplicação esta rodando
-		while( !quit_game )
+		while( l_quit_game != 1 )
 		{
 			// Cuida dos eventos na fila
 			while( SDL_PollEvent( &e ) != 0 )
@@ -111,7 +124,7 @@ int main(int argc, char *argv[])
 				// Usuario deseja sair
 				if( e.type == SDL_QUIT )
 				{
-					quit = true;
+					l_quit_game = 1;
 				}
 			}
 
@@ -142,13 +155,12 @@ int main(int argc, char *argv[])
 	return close_game();
 }
 
-SDL_Surface* load_and_optimizeSurface( char* l_Path )
+void load_Texture( char* l_Path )
 {
-	//Imagem PNG e Imagem PNG otimizada
+	//Imagem PNG em surface e textura
 	SDL_Surface* l_loadedSurface = NULL;
-	SDL_Surface* l_optimizedSurface = NULL;
-	
-	// Carrega a imagem de uma localizacao
+
+	// Carrega a imagem de um diretorio
 	l_loadedSurface = IMG_Load( l_Path );
 	if( l_loadedSurface == NULL )
 	{
@@ -157,36 +169,44 @@ SDL_Surface* load_and_optimizeSurface( char* l_Path )
 	
 	else
 	{
-		// Converte a surface para o formato da tela
-		l_optimizedSurface = SDL_ConvertSurface( l_loadedSurface, g_ScreenSurface->format, 0 );
-		if( l_optimizedSurface == NULL )
+		// Gera textura atraves da surface
+        g_Texture = SDL_CreateTextureFromSurface( g_Renderer, l_loadedSurface );
+		if( g_Texture == NULL )
 		{
-			printf( "Unable to optimize image %s! SDL Error: %s\n", l_Path, SDL_GetError() );
+			printf( "Unable to create texture from %s! SDL Error: %s\n", l_path, SDL_GetError() );
 		}
 
 		// Se livra da surface que nao sera usada
 		SDL_FreeSurface( l_loadedSurface );
 	}
-
-	return l_optimizedSurface;
 }
 
-void apply_update_Surface (SDL_Surface* l_Surface)
+void apply_Texture()
 {
-	// Aplica a imagem 
-	SDL_BlitSurface( l_Surface, NULL, g_ScreenSurface, NULL );
-		
-	// Atualiza a surface
-	SDL_UpdateWindowSurface( g_Window );
+	// Limpa tela
+	SDL_RenderClear( g_Renderer );
+
+	// Renderiza a textura a tela
+	SDL_RenderCopy( g_Renderer, g_Texture, NULL, NULL );
+
+	// Atualiza a tela
+	SDL_RenderPresent( g_Renderer );
 }
 
 int close_game()
 {
+	// Libera imagens carregadas
+	SDL_DestroyTexture( g_Texture );
+	g_Texture = NULL;
+
 	// Destroi a janela
+	SDL_DestroyRenderer( g_Renderer );
 	SDL_DestroyWindow( g_Window );
 	g_Window = NULL;
+	g_Renderer = NULL;
 
 	// Sai dos subsistemas do SDL
+	IMG_Quit();
 	SDL_Quit();
 	
 	return 0;

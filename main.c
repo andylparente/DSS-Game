93a9f8bf222c80404cc5b4c35204d72ca5360802
Andrei Leone Parente Silva <andreiparente@gmail.com>
2016-06-03 23:32:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
+#include <SDL2/SDL_image.h>
 
 // Dimensoes constantes da tela
 const int SCREEN_WIDTH = 800;
